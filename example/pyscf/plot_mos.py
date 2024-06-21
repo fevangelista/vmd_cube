079697d939c367bd7d041893a77d6c14879354fa
Brian Zhao <zijun.zhao@emory.edu>
2024-06-21 16:21:35 -0400

    Handle no symmetry

--- example/pyscf/plot_mos.py
+++ example/pyscf/plot_mos.py
@@ -6,21 +6,25 @@ def main():
     O
     H 1 1.0
     H 1 1.0 2 104.5
-    """, basis='sto-3g',symmetry='c2v')
+    """, basis='sto-3g',symmetry=False)
 
     mf = pyscf.scf.RHF(mol)
     mf.kernel()
 
-    irrep = mf.mo_coeff.orbsym
-    irrep_indices = irrep.copy()
+    if (mol.symmetry == False):
+        for i in range(mol.nao):
+            pyscf.tools.cubegen.orbital(mol, f'Psi_a_{i}_{i}-A.cube', mf.mo_coeff[:,i])
+    else:
+        irrep = mf.mo_coeff.orbsym
+        irrep_indices = irrep.copy()
 
-    irrep_indices[np.argwhere(irrep==0).flatten()] = np.arange(1, 1+np.sum(irrep==0))
-    irrep_indices[np.argwhere(irrep==1).flatten()] = np.arange(1, 1+np.sum(irrep==1))
+        irrep_indices[np.argwhere(irrep==0).flatten()] = np.arange(1, 1+np.sum(irrep==0))
+        irrep_indices[np.argwhere(irrep==1).flatten()] = np.arange(1, 1+np.sum(irrep==1))
 
-    irrep_dict = {mol.irrep_id[i]:mol.irrep_name[i] for i in range(len(mol.irrep_id))}
+        irrep_dict = {mol.irrep_id[i]:mol.irrep_name[i] for i in range(len(mol.irrep_id))}
 
-    for i in range(mol.nao):
-        pyscf.tools.cubegen.orbital(mol, f'Psi_a_{i}_{irrep_indices[i]}-{irrep_dict[mf.mo_coeff.orbsym[i]]}.cube', mf.mo_coeff[:,i])
+        for i in range(mol.nao):
+            pyscf.tools.cubegen.orbital(mol, f'Psi_a_{i}_{irrep_indices[i]}-{irrep_dict[mf.mo_coeff.orbsym[i]]}.cube', mf.mo_coeff[:,i])
 
 if __name__ == '__main__':
     main()
